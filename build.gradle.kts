488a52d5b9db8668a307e8482d60fb2256939498
ponktacology <ponktacology@mailfence.com>
2026-03-23 18:28:21 +0100

    remove unused dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     implementation(libs.ktor.client.content.negotiation)
     implementation(libs.logback.classic)
     implementation(libs.ktor.server.config.yaml)
+    implementation("io.ktor:ktor-client-logging:3.4.1")
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
 }
